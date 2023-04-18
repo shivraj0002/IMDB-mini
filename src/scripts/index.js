67150b14822bacda94022c26a38ee68b86e33b8a
shivraj0002 <95290113+shivraj0002@users.noreply.github.com>
2023-04-18 23:15:53 +0530

    initialized project and create static html page and styles

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,24 @@
-// script file for main page of website
+// script file for main page of website.
+const hambergerMenu = document.getElementsByClassName("menu")[0];
+const hambergerIcon = document.getElementsByClassName("dropdown")[0];
+
+hambergerIcon.addEventListener("click", () => {
+    hambergerMenu.classList.toggle("d-flex");
+    hambergerMenu.classList.toggle("d-none");
+})
+
+const searchBar = document.querySelector("#search-bar");
+
+
+async function getMoviesList(key) {
+    let url = `https://www.omdbapi.com/?s=${key}&?type=sereis&apikey=e10a7d33`;
+
+    let req = await fetch(url);
+    let data = await req.json();
+    console.log(data.Response);
+}
+
+searchBar.addEventListener("keyup", function (e) {
+    console.log(e.target.value);
+    getMoviesList(e.target.value)
+})

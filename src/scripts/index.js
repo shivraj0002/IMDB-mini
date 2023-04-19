9122b51b1bb07314206c02a558fffc619f4722e1
shivraj0002 <95290113+shivraj0002@users.noreply.github.com>
2023-04-19 13:39:20 +0530

    favorite movie add and delete with localstorage is added

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,7 +9,7 @@ hambergerIcon.addEventListener("click", () => {
 
 const searchBar = document.querySelector("#search-bar");
 const searchResults = document.getElementById("results");
-
+const fevorites = document.getElementById("fevoriteMovies");
 
 async function getMoviesList(key) {
     let url = `https://www.omdbapi.com/?s=${key}&?type=sereis&apikey=e10a7d33`;
@@ -17,7 +17,7 @@ async function getMoviesList(key) {
     try {
         const response = await fetch(url);
         const data = await response.json();
-        // console.table(data?.Search);
+        console.table(data?.Search);
         if (data.Search) {
             renderSearchResults(data.Search)
         } else {
@@ -50,17 +50,17 @@ function renderSearchResults(array) {
             <div class="movie-card-header">
                 <img src=${data.Poster} 
                 alt="Movie poster"
-                data-id = "${data.Title} "
+                data-id = "${data.Title}"
                 class="movie-card-poster">
             </div>
             <div class="movie-card-body">
                 <p class="movie-release-date">
                     <i class="fa-solid fa-calendar-days"></i>
-                    27-10-23
+                    ${data.Year}
                 </p>
                 <h4 
                 class="movie-card-title"
-                data-id = "${data.Title} ">
+                data-id="${data.Title}">
                 ${data.Title}</h4>
 
             </div>
@@ -68,10 +68,10 @@ function renderSearchResults(array) {
             <div class="movie-card-footer">
                 <p class="card-text">
                     <i class="fa-solid fa-star">
-                        <span id="rating">&nbsp;${data.imdbRating}</span>
+                        <span id="rating">&nbsp;${data.Type}</span>
                     </i>
-                    <button class="fav-btn">
-                        <i class="fa-solid fa-heart"></i>
+                    <button class="fav-btn" id="add-fev"  data-id="${data.imdbID} ">
+                        <i class="fa-solid fa-heart" id="add-fevo"  data-id="${data.imdbID}"></i>
                     </button>
                 </p>
             </div>
@@ -79,3 +79,91 @@ function renderSearchResults(array) {
   `
     })
 }
+
+function renderFevoriteList() {
+    const storedFeviriteList = localStorage.getItem('favorites');
+    if (storedFeviriteList !== null) {
+        const currentFevoriteList = JSON.parse(storedFeviriteList);
+        fevorites.innerHTML = "";
+        currentFevoriteList.map((data) => {
+            fevorites.innerHTML = fevorites.innerHTML + `<a href="">
+            <div class="fev-movie-card-container">
+                <div class="fev-movie-poster"><img
+                        src="${data.Poster}"
+                        alt="Poster Image"></div>
+                        </a>
+                        <a href="">
+                <div class="fev-movie-title">
+                    <h4 class="fev-movie-card-title">${data.Title}</h4>
+                    <p class="fev-movie-release-date">
+                        <i class="fa-solid fa-calendar-days"></i>
+                        ${data.Year}
+                    </p>
+                </div>
+                </a>
+                <div class="fev-movie-delete-icon" >
+                    <i class="fa-solid fa-trash-can" id="del-fev" data-id="${data.imdbID}"></i>
+                </div>
+            </div> `
+
+        })
+
+    }
+}
+
+function favoriteMovieAdd(movie) {
+    const storedFeviriteList = localStorage.getItem('favorites');
+    if (storedFeviriteList !== null) {
+        const currentFevoriteList = JSON.parse(storedFeviriteList);
+        currentFevoriteList.unshift(movie);
+        localStorage.setItem('favorites', JSON.stringify(currentFevoriteList));
+    } else {
+        let newFavoriteMovieList = []
+        newFavoriteMovieList.push(movie);
+        localStorage.setItem('favorites', JSON.stringify(newFavoriteMovieList));
+    }
+    renderFevoriteList()
+}
+function favoriteMovieDelete(id) {
+    const storedFeviriteList = localStorage.getItem('favorites');
+    if (storedFeviriteList !== null) {
+        const currentFevoriteList = JSON.parse(storedFeviriteList);
+        let newFavoriteList = currentFevoriteList.filter(movie => movie.imdbID != id)
+        localStorage.setItem('favorites', JSON.stringify(newFavoriteList));
+    }
+    renderFevoriteList()
+}
+
+async function favoriteMovieGet(searchValue) {
+    const url = `https://www.omdbapi.com/?i=${searchValue}&?type=sereis&apikey=e10a7d33`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Something went wrong");
+        }
+        const data = await response.json();
+        // console.log(data);
+        favoriteMovieAdd(data)
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+(
+    function () {
+        renderFevoriteList();
+    }
+)();
+
+document.addEventListener("click", function (e) {
+    // console.log(e.target);
+    if (e.target.id == "add-fev" || e.target.id == "add-fevo") {
+        let id = e.target.dataset.id;
+        console.log(id);
+        favoriteMovieGet(id)
+    } else if (e.target.id == "del-fev") {
+        let id = e.target.dataset.id;
+        console.log(id);
+        favoriteMovieDelete(id)
+    }
+});
